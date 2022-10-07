311027873c5cd6b019e7bfd6337681776513d513
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2022-10-07 14:24:13 -0600

    updates

--- gilded-rose-refactoring-kata/README.md
+++ gilded-rose-refactoring-kata/README.md
@@ -1,3 +1,31 @@
 # Gilded Rose Kata
 
+https://kata-log.rocks/gilded-rose-kata
+
 https://github.com/emilybache/GildedRose-Refactoring-Kata
+
+# Introduction
+
+- All items have a SellIn value which denotes the number of days we have to sell the item
+- All items have a Quality value which denotes how valuable the item is
+- At the end of each day our system lowers both values for every item
+- Once the sell by date has passed, Quality degrades twice as fast
+- The Quality of an item is never negative
+- The Quality of an item is never more than 50
+- “Aged Brie” actually increases in Quality the older it gets
+- “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+  - “Sulfuras” is a legendary item and as such its Quality is 80 and it never alters.
+- “Backstage passes”, increases in Quality as its SellIn value approaches;
+  - Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+  - Quality drops to 0 after the concert
+
+# Your Task
+
+We have recently signed a supplier of conjured items. This requires an update to our system:
+
+“Conjured” items degrade in Quality twice as fast as normal items
+
+# Rules
+
+Feel free to make any changes to the UpdateQuality method and add any new code as long as everything still works correctly.
+However, do not alter the Item class or Items property (you can make the UpdateQuality method and Items property static if you like).
--- gilded-rose-refactoring-kata/test_gilded_rose.py
+++ gilded-rose-refactoring-kata/test_gilded_rose.py
@@ -1,16 +1,136 @@
-# -*- coding: utf-8 -*-
-import unittest
-
+import pytest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert "foo" == items[0].name
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_regular_items_decrease_by_one():
+    gilded_rose = GildedRose([Item("+5 Dexterity Vest", 10, 20)])
+    expected = {"sell_in": 9, "quality": 19}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
+
+
+def test_quality_goes_up_for_improving_products():
+    gilded_rose = GildedRose(
+        [
+            Item("Aged Brie", 20, 30),
+            Item("Backstage passes to a TAFKAL80ETC concert", 20, 30),
+        ]
+    )
+    expected = [
+        {"sell_in": 19, "quality": 31},
+        {"sell_in": 19, "quality": 31},
+    ]
+
+    gilded_rose.update_quality()
+
+    for index, expectation in enumerate(expected):
+        item = gilded_rose.items[index]
+        assert item.quality == expectation["quality"]
+        assert item.sell_in == expectation["sell_in"]
+
+
+def test_quality_goes_up_by_two_for_backstage_passes_with_10_days_or_less_left():
+    gilded_rose = GildedRose(
+        [
+            Item("Backstage passes to a TAFKAL80ETC concert", 8, 30),
+        ]
+    )
+    expected = {"sell_in": 7, "quality": 32}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
+
+
+def test_quality_goes_up_by_three_for_backstage_passes_with_5_days_or_less_left():
+    gilded_rose = GildedRose(
+        [
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 15),
+        ]
+    )
+    expected = {"sell_in": 4, "quality": 18}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
+
+
+def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by():
+    gilded_rose = GildedRose(
+        [Item("+5 Dexterity Vest", 0, 20), Item("Conjured Mana Cake", 0, 6)]
+    )
+    expected = [
+        {"sell_in": -1, "quality": 18},
+        {"sell_in": -1, "quality": 4},
+    ]
+
+    gilded_rose.update_quality()
+
+    for index, expectation in enumerate(expected):
+        item = gilded_rose.items[index]
+        assert item.quality == expectation["quality"]
+        assert item.sell_in == expectation["sell_in"]
+
+
+def test_backstage_passes_goes_to_quality_zero_after_sell_by():
+    gilded_rose = GildedRose(
+        [
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
+        ]
+    )
+    expected = {"sell_in": -1, "quality": 0}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
+
+
+def test_sulfuras_the_immutable():
+    gilded_rose = GildedRose([Item("Sulfuras, Hand of Ragnaros", 0, 80)])
+    expected = {"sell_in": 0, "quality": 80}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
+
+
+def test_quality_does_not_increase_past_50():
+    gilded_rose = GildedRose([Item("Aged Brie", 4, 49)])
+    expected = {"sell_in": 3, "quality": 50}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
+
+
+@pytest.mark.skip(reason="new feature")
+def test_conjured_items_decrease_in_quality_twice_as_fast():
+    gilded_rose = GildedRose([Item("Conjured Mana Cake", 3, 6)])
+    expected = {"sell_in": 2, "quality": 2}
+
+    gilded_rose.update_quality()
+
+    item = gilded_rose.items[0]
+    assert item.quality == expected["quality"]
+    assert item.sell_in == expected["sell_in"]
